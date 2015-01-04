7ba7a41d74d34f920363d8f7f53d803edc94d56c
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-01-04 18:07:01 +0200

    Sign out added.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var home = require('../controllers/home'),
-	login = require('../controllers/login');
+	login = require('../controllers/login'),
+	signout = require('../controllers/signout');
 
 module.exports = function (app) {
 	app.route('/')
@@ -9,4 +10,7 @@ module.exports = function (app) {
 
 	app.route('/login')
 		.get(login.get);
+
+	app.route('/signout')
+		.get(signout.get);
 };
