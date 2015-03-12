62ee4bacea04695cc9db2da338b062e11d2fcd90
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-03-12 22:58:14 +0200

    Example using HTTPS SiteGate.

--- config/config.js
+++ config/config.js
@@ -2,18 +2,22 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/..');
 var env = process.env.NODE_ENV || 'development';
 
+if (env === 'development') {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
 var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'sesame-client-example'
+      name: 'sitegate-client-example'
     },
     port: 3001,
     db: 'mongodb://localhost/sitegate-client-example-development',
-    sessionSecret: 'sesame-client-example',
+    sessionSecret: 'sitegate-client-example',
     sessionCollection: 'sessions',
     sitegate: {
-      domain: 'http://account.sitegatedev.com:3000',
+      domain: 'https://account.sitegatedev.com:3000',
       clientId: 'K2fqgHNhGFGKBQtE7V7o',
       clientSecret: 'AgmkWLx5tv2QfzUEYAvdAvdsrcG70oK7xl2vmCUS',
       callbackURL: 'http://sitegatedev.com:3001/auth/sitegate/callback'
@@ -23,22 +27,22 @@ var config = {
   test: {
     root: rootPath,
     app: {
-      name: 'sesame-client-example'
+      name: 'sitegate-client-example'
     },
     port: 3001,
     db: 'mongodb://localhost/sitegate-client-example-test',
-    sessionSecret: 'sesame-client-example',
+    sessionSecret: 'sitegate-client-example',
     sessionCollection: 'sessions'
   },
 
   production: {
     root: rootPath,
     app: {
-      name: 'sesame-client-example'
+      name: 'sitegate-client-example'
     },
     port: 3001,
     db: 'mongodb://localhost/sitegate-client-example-production',
-    sessionSecret: 'sesame-client-example',
+    sessionSecret: 'sitegate-client-example',
     sessionCollection: 'sessions'
   }
 };
